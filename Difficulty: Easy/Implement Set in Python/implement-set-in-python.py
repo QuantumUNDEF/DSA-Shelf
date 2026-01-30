709af2b845e0aaa8d250c4962adf576f7543e976
Kaushendra Singh <87465098+QuantumUNDEF@users.noreply.github.com>
2026-01-30 22:22:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Set in Python/implement-set-in-python.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+# Function to insert
+# no return should be there, and no print statement
+def insert(s, element):
+    s.add(element)
+    
+def remove_from_set(s, element):
+    s.remove(element)
+  
+def sum_set(s):
+    return sum(s)
+    
+
+
